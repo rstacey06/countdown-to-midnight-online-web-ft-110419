63a8f22abf6b9695849a70cb8ac2fd60f302cae6
Burbage Stacey <rstacey06@gmail.com>
2019-11-06 19:48:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,6 @@
 def countdown(number)
-  while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
-end
- "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(number)
-  while number > 0 
-  puts "#{number} SECOND(S)"
-  sleep 1  
-  number -= 1 
-end 
-"HAPPY NEW YEAR!"
+ number = 10 
+ while number > 1
+ puts "#{number} SECOND(S)!"
+ x -= 1 
 end
