14763059e82e5164febc059f871ed63a790c1bba
Burbage Stacey <rstacey06@gmail.com>
2019-11-06 19:54:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,22 +3,6 @@ def countdown(number)
  puts "#{number} SECOND(S)!"
  number -= 1
 end
-returns "HAPPY NEW YEAR!"
-end
-
-def countdown(number)
-  while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
-end
- "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(number)
-  while number > 0 
-  puts "#{number} SECOND(S)"
-  sleep 1  
-  number -= 1 
-end 
 "HAPPY NEW YEAR!"
 end
+
